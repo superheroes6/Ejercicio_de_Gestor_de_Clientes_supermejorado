45085cd1aab85de5f4c9c79457555109bc565788
santibox888@gmail.com <santibox888@gmail.com>
2025-04-08 19:56:52 +0200

    Cambio2

--- helpers.py
+++ helpers.py
@@ -10,3 +10,15 @@ def leer_texto(longitud_min=0, longitud_max=100, mensaje=None):
         texto = input("> ")
         if len(texto) >= longitud_min and len(texto) <= longitud_max:
             return texto
+        
+import re
+
+def dni_valido(dni, lista):
+    if not re.match(r'^[0-9]{8}[A-Z]$', dni):
+        print("DNI incorrecto, debe cumplir el formato.")
+        return False
+    for cliente in lista:
+        if cliente.dni == dni:
+            print("DNI utilizado por otro cliente.")
+            return False
+    return True
